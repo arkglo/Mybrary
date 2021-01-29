e83c596f39b8fb3d3b83fc49f7d05e2d5dcd0d24
mark.glover <mark.glover@airborne.aero>
2021-01-29 16:16:52 +0000

    Tweaked Mongoose connection

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 const { once } = require('nodemon')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connect to Mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 
 
